9e76ffc81c46b602a34f5556d441866740cd239e
Alexandr Zuev <zuev.alexandr.d@yandex.ru>
2023-05-20 01:45:17 +0300

    Проект №4 Финальная версия

--- index.js
+++ index.js
@@ -8,50 +8,43 @@ const nameInputSubTitleEl = document.querySelector('#name-input-subtitle');
 const editFormEl = document.querySelector('#edit-form');
 
 openPopupButtonEl.addEventListener('click', function () {
-    openPopup(editPopupEl);
-  });
+  openPopup(editPopupEl);
+});
 
-  function openPopup(popupEl) {
-    popupEl.classList.add('popup_is-opened');
-  }
+function openPopup(popupEl) {
+  popupEl.classList.add('popup_is-opened');
+}
 
 closePopupButtonEl.addEventListener('click', function () {
-    closePopup(editPopupEl);
-  });
-  
-  function closePopup(popupEl) {
-    popupEl.classList.remove('popup_is-opened');
-  }
-  
-  nameInputTitleEl.value = profileTitleEl.textContent;
-  nameInputSubTitleEl.value = profileSubTitleEl.textContent;
- 
-  
-  editFormEl.addEventListener('submit', function (event) {
-    event.preventDefault();
-  
+  closePopup(editPopupEl);
+});
+
+function closePopup(popupEl) {
+  popupEl.classList.remove('popup_is-opened');
+}
+
+nameInputTitleEl.value = profileTitleEl.textContent;
+nameInputSubTitleEl.value = profileSubTitleEl.textContent;
+
+editFormEl.addEventListener('submit', function (event) {
+  event.preventDefault();
+
   profileTitleEl.textContent = nameInputTitleEl.value;
   profileSubTitleEl.textContent = nameInputSubTitleEl.value;
-  
-    closePopup(editPopupEl);
+
+  closePopup(editPopupEl);
+});
+
+const buttonsLike = document.querySelectorAll('.like-button');
+let isImageChanged = false;
+buttonsLike.forEach(function (button) {
+  button.addEventListener('click', function () {
+    if (isImageChanged) {
+      button.style.backgroundImage = "url('../../images/likeButton.svg')";
+      isImageChanged = false;
+    } else {
+      button.style.backgroundImage = "url('../../images/likeButton_active.svg')";
+      isImageChanged = true;
+    }
   });
-
-  const buttonsLike = document.querySelectorAll('.like-button');
-  let isImageChanged = false;
-  buttonsLike.forEach(function(button) {
-    button.addEventListener('click', function() {
-      if (isImageChanged) {
-        button.style.backgroundImage = "url('../../images/likeButton.svg')";
-        isImageChanged = false;
-      } else {
-        button.style.backgroundImage = "url('../../images/likeButton_active.svg')";
-        isImageChanged = true;
-      }
-    });
-  });
-  
-  
-
-  
-  
-
+});
